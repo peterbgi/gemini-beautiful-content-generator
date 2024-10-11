7df4e6e9851ee9d06c7d79195f154f1e502b9f03
Peter Bagi <peti.bagi@icloud.com>
2024-10-11 17:06:45 +0200

    db push done

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+    dialect: 'postgresql',
+    schema: './utils/db/schema.ts',
+    out: './drizzle',
+
+    dbCredentials: {
+        url: "postgresql://Gemini_AI_Contents_owner:2YWf3FQSGMLs@ep-old-glade-a2fkobgd.eu-central-1.aws.neon.tech/Gemini_AI_Contents?sslmode=require",
+        connectionString: "postgresql://Gemini_AI_Contents_owner:2YWf3FQSGMLs@ep-old-glade-a2fkobgd.eu-central-1.aws.neon.tech/Gemini_AI_Contents?sslmode=require",
+    }
+}
